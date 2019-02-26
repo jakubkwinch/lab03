6bb3ca213933bc60ed8a1631f5f8d41461fc47d0
jakubkwinch <36926437+jakubkwinch@users.noreply.github.com>
2019-02-26 11:47:52 -0500

    Create Lab03.java

---
+++ Lab03.java
@@ -0,0 +1,34 @@
+package lab03;
+import java.util.Scanner; 
+public class Lab03 {
+  public static void main(String[] args) {
+    //Scanner
+    Scanner input = new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
+    
+    //Ask for integer #1
+    System.out.print("Please enter an integer between 0-99: ");
+    int integer1 = input.nextInt();
+
+    if (integer1  < 0 || integer1 > 99) {
+        System.out.print("Outside range. Please enter an integer between 0-99: ");
+    }   else {
+        System.out.print("Please enter an integer between 0-99: ");
+        
+    }
+    
+    int integer2 = input.nextInt();
+    
+    if (integer2  < 0 || integer2 > 99) {
+        System.out.print("Outside range. Closing program");
+    }
+    
+    
+   
+   
+    
+    }
+     
+  }
+    
+  
